521499d2608630d7f95ac27dd656d0806d1bcb66
CHINYAMG <chinyamg@zra.org.zm>
2022-10-30 04:42:15 +0200

    feat: added route for swagger documentation

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//documentation and api testing using swagger
+Route::get('/docs', function () {
+    return view('swagger.index');
+});
